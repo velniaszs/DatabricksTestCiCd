57389165ac93a7fac2e4cc3871ad409ee2a4fceb
abaubinas <abaubinas@microsoft.com>
2022-06-17 11:18:03 +0200

    add a comment

--- common/module_a.py
+++ common/module_a.py
@@ -1,5 +1,5 @@
 from pyspark.sql import DataFrame
 from pyspark.sql import functions as F
-
+#this is my comment
 def add_mount(df: DataFrame, var_amount):
   return df.withColumn("Amount", F.lit(var_amount))
